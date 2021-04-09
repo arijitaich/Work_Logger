bed43c0e12e41f22ab676e785a068ca84105be36
Arijit Aich <a2.arijitaich@gmail.com>
2021-04-09 13:09:39 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,36 @@
+import subprocess, pathlib
+import time
+import os
+
+print("\n")
+print("Welcome to X2Chat")
+print("Version 1.0 launched on 30th March 2021")
+print("Author: Arijit Aich")
+print()
+x = input("Enter username: ")
+y = input("Enter passkey: ")
+
+print("Connecting.", end="\r")
+time.sleep(1)
+print("Connecting..", end="\r")
+time.sleep(1)
+print("Connecting...", end="\r")
+time.sleep(1)
+print("Connected.   ", end="\r") 
+
+print()
+# print()
+time.sleep(1)
+
+r = 1
+
+while (r==1):
+    # Reading File   
+    f = open("a1.txt", "r") 
+    x = f.read()
+    print(x, end="\r")
+    time.sleep(0.5)
+    os.system( 'clear' )
+
+
+
